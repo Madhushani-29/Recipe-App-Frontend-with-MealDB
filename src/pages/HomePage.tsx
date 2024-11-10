c3f5323b67b9d0237f08eebd069320a004a912b5
madhushani-29 <madhushani2904@gmail.com>
2024-11-11 00:11:57 +0530

    logout feature

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,21 @@
 import icons from "@/constants/icons"
+import { useLogout } from "@/hooks/useLogout";
 import { LogOut } from "lucide-react"
 
 const HomePage = () => {
+    const logout = useLogout();
+    const onClickLogout = () => {
+        const confirm = window.confirm("Are you sure want to logout?");
+        if (confirm) {
+            logout();
+        }
+    }
     return (
         <div>
             <div className='flex justify-between items-center h-16 bg-red-300 px-24'>
                 <img src={icons.themeIcon} alt="theme icon" className="w-30" />
                 <div>Icons</div>
-                <LogOut className="" />
+                <LogOut onClick={onClickLogout} />
             </div>
         </div>
     )
